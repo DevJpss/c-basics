934619b27bb13598c776fe65a16293f1b256fd95
Koshida <joaopaulodsspessoal@gmail.com>
2026-02-06 23:56:11 -0300

    Add variables example in variables.c

---
+++ 02-variables/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int idade = 22;
+    float altura = 1.75;
+    char inicial = 'K';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Inicial: %c\n", inicial);
+
+    return 0;
+}
